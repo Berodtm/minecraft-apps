4196de84e434145a680b2c2b1c8b540d98f61b14
Rob Dunn <dunnrob@hotmail.co.uk>
2024-08-31 21:39:23 +0100

    destructured remaining props on table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 
-function Table(props) {
-    return (
-      <table className="table-auto text-left border-spacing-2 w-full border border-gray-300">
-        <thead>
-          <tr className="bg-gray-200">
-            <th scope='col' className="px-4 py-2 border"> {props.colHeadA} </th>
-            <th scope='col' className="px-4 py-2 border"> {props.colHeadB} </th>
-            <th scope='col' className="px-4 py-2 border"> {props.colHeadC} </th>
-            <th scope='col' className="px-4 py-2 border"> {props.colHeadD} </th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.filteredSearch.map((food) => {
-            return (
-              <tr key={food.id} className="hover:bg-gray-100">
-                <td scope='col' className="px-4 py-2 border"> {food.id} </td>
-                <td scope='col' className="px-4 py-2 border"> {food.name} </td>
-                <td scope='col' className="px-4 py-2 border"> {food.hearts} </td>
-                <td scope='col' className="px-4 py-2 border"> {food.other} </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
+function Table({ colHeadA, colHeadB, colHeadC, colHeadD, filteredSearch }) {
+  return (
+    <table className="table-auto text-left border-spacing-2 w-full border border-gray-300">
+      <thead>
+        <tr className="bg-gray-200">
+          <th scope='col' className="px-4 py-2 border"> {colHeadA} </th>
+          <th scope='col' className="px-4 py-2 border"> {colHeadB} </th>
+          <th scope='col' className="px-4 py-2 border"> {colHeadC} </th>
+          <th scope='col' className="px-4 py-2 border"> {colHeadD} </th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredSearch.map((food) => (
+            <tr key={food.id} className="hover:bg-gray-100">
+              <td scope='col' className="px-4 py-2 border"> {food.id} </td>
+              <td scope='col' className="px-4 py-2 border"> {food.name} </td>
+              <td scope='col' className="px-4 py-2 border"> {food.hearts} </td>
+              <td scope='col' className="px-4 py-2 border"> {food.other} </td>
+            </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
+
 export default Table;
